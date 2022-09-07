e8910a2679460b3331f419f32ade513af9074236
Maxime Boucher <maxime@maximeboucher.com>
2022-09-07 08:11:04 -0600

    Work on sidebar and dashboard UI

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar() {
     return(
-        <div className="flex flex-col justify-between items-stretch border-r w-48 h-screen fixed">
+        <div className="flex flex-col justify-between items-stretch border-r w-56 h-screen fixed">
             <div>
                 <h1 className="text-4xl text-sky-400 font-bold p-6">Logo</h1>
                 <nav className="flex flex-col">
@@ -11,8 +11,8 @@ export default function Sidebar() {
                     <Link to="/settings" className="p-2 mx-1 rounded-md hover:bg-sky-400 hover:text-white font-medium">Settings</Link>
                 </nav>
             </div>
-            <div className="flex items-center">
-                <img src="../profile-fallback.jpg" alt="Maxime Boucher" className="w-10 rounded-full" />
+            <div className="flex justify-start items-center gap-2 p-4 border-t">
+                <img className="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1729&q=80" alt="Maxime Boucher" />
                 <div>
                     <p className="text-sm font-medium">Maxime Boucher</p>
                     <p className="text-xs text-gray-600">View Account</p>
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -2,8 +2,21 @@ import PageTitle from "../components/PageTitle";
 
 export default function DashboardPage() {
     return(
-        <section>
-            <PageTitle title="Dashboard" />
-        </section>
+        <main className="ml-56 py-16 px-24">
+            <header className="flex justify-between items-end">
+                <PageTitle title="Dashboard" />
+                <div>
+                    <select>
+                        <option>Day</option>
+                        <option>Week</option>
+                        <option>Month</option>
+                        <option>Quarter</option>
+                        <option>Year</option>
+                    </select>
+                    <button>Today</button>
+                </div>
+            </header>
+            <div className="bg-slate-400 rounded-md w-full h-56"></div>
+        </main>
     )
 }
